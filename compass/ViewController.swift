44dde0f62e22caae9869f42ab97fae6fad1b6289
Mac561 <maccooper561@gmail.com>
2020-03-09 16:25:03 -0700

    Initial Commit

---
+++ compass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  compass
+//
+//  Created by Mac Cooper on 3/9/20.
+//  Copyright © 2020 Mac Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
